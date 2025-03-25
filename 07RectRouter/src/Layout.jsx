3af7eeff4e3c936de354f5b6621aa93081b5d1b8
RoutPartha <parthasarathirout408@gmail.com>
2025-03-26 01:25:47 +0530

    added

---
+++ 07RectRouter/src/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router'
+function Layout() {
+  return (
+    <>
+    <Header/>
+    <Outlet/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Layout
+//Note--> we write outlet in between Header and Footer because 
+// the Header and Footer will be same but the containt under the outlet will be changed
